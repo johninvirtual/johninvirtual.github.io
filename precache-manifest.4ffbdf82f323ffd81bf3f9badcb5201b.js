a9b0c5db39893991c721c110696ebd7741dafb6a
John Marshal <john.marshal@smartenovations.com>
2020-03-31 09:44:33 +0530

    Updates

---
+++ precache-manifest.4ffbdf82f323ffd81bf3f9badcb5201b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba630fc34ea1d94aa0c5a88b881b6d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "747b508e92e27d704762",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eedfa58da2932c09368d",
+    "url": "/static/js/2.7b587d56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b587d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747b508e92e27d704762",
+    "url": "/static/js/main.da34b90d.chunk.js"
+  },
+  {
+    "revision": "e13ef31792168953d63f",
+    "url": "/static/js/runtime-main.a1d3201b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
